072364c81ed91f98fd57408dce06e5053bb2dc3c
ananta pothal <anantapothal@anantas-MacBook-Pro.local>
2024-01-15 17:25:27 +0530

    added bootstrap and removed unwanted codes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <button type="button" class="btn btn-primary">Base class</button>
+
+      <div className='alert al'> this is the bootstrap header</div>
+      <h1>this is the app component</h1>
+    </>
   );
 }
 
